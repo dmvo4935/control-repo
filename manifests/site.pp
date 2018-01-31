b838a607f6871035ba4c3a2c87a395923e05fa4f
dmvo4935 <33449452+dmvo4935@users.noreply.github.com>
2018-01-31 17:34:42 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -52,11 +52,11 @@ node 'ec2amaz-pcdm8f8.eu-central-1.compute.internal' {
 
 node 'ec2amaz-05d23ld.eu-central-1.compute.internal' {
    dsc_xActiveDirectory {'xADDomain':
-   dsc_DomainName => 'mydomain.local',
-   dsc_SafemodeAdministratorPassword: => 'Supersecret#123',
+   dsc_DomainName                    => 'mydomain.local',
+   dsc_SafemodeAdministratorPassword => 'Supersecret#123',
    }
   
    reboot {'dsc_reboot':
      message => 'DSC has requested a reboot',
-     when => 'pending'
+     when    => 'pending'
 }
