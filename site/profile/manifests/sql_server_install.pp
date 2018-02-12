f33b6dc29583ede3528be021f691e9b38a6e6979
root <root@instance-puppet-enterprise.c.eternal-centaur-185911.internal>
2018-02-12 14:03:52 +0000

    bug fixing

--- site/profile/manifests/sql_server_install.pp
+++ site/profile/manifests/sql_server_install.pp
@@ -21,57 +21,57 @@ dsc_file {'test file':
     dsc_destinationpath => '${sql_sorce}\testfile',
   } 
 
-exec {'Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force': 
-     provider => powershell, 
-    } -> 
+#exec {'Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force': 
+#     provider => powershell, 
+#    } -> 
 
-exec {"Set-PSRepository -Name \"PSGallery\" -InstallationPolicy Trusted -SourceLocation 'https://www.powershellgallery.com/api/v2/'": provider => powershell, } ->
+#exec {"Set-PSRepository -Name \"PSGallery\" -InstallationPolicy Trusted -SourceLocation 'https://www.powershellgallery.com/api/v2/'": provider => powershell, } ->
 
-exec {'Install SqlserverDsc module':
-     command  => 'Find-Module -Name SqlServerDsc -Repository PSGallery | Install-Module',
-     provider => powershell, 
-    } ->
+#exec {'Install SqlserverDsc module':
+#     command  => 'Find-Module -Name SqlServerDsc -Repository PSGallery | Install-Module',
+#     provider => powershell, 
+#    } ->
 
 
-dsc_sqlsetup {'SqlServerDsc':
-   dsc_action       =>  'Install',
-   dsc_instancename =>  'MSSQLSERVER',
-   dsc_features     =>  'SQLENGINE,AS',
-   dsc_sourcepath   => $sql_source,
-   dsc_sourcecredential => {
-    'user'      =>  'mydomain\administrator',
-    'password'  =>  'Supersecret#123',
-    },
-   dsc_forcereboot  => 'false',
-   dsc_installshareddir     => 'C:\Program Files\Microsoft SQL Server',
-   dsc_installsharedwowdir  => 'C:\Program Files (x86)\Microsoft SQL Server',
-   dsc_instancedir          => 'C:\Program Files\Microsoft SQL Server',
-   dsc_installsqldatadir    => 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Data',
-   dsc_sqluserdbdir         => 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Data',
-   dsc_sqluserdblogdir      => 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Data',
-   dsc_sqltempdbdir         => 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Data',
-   dsc_sqltempdblogdir      => 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Data',
-   dsc_sqlbackupdir         => 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Backup',
-   dsc_asconfigdir          => 'C:\MSOLAP\Config',
-   dsc_asdatadir            => 'C:\MSOLAP\Data',
-   dsc_aslogdir             => 'C:\MSOLAP\Log',
-   dsc_asbackupdir          => 'C:\MSOLAP\Backup',
-   dsc_astempdir            => 'C:\MSOLAP\Temp',
-   dsc_sqlcollation         => 'SQL_Latin1_General_CP1_CI_AS',
-   dsc_sqlsvcaccount        => {
-     'user'      => 'mydomain\administrator',
-     'password'  => 'Supersecret#123',
-    },
-   dsc_agtsvcaccount        => {
-     'user'      => 'mydomain\administrator',
-     'password'  => 'Supersecret#123',
-    }, 
-   dsc_assvcaccount        => {
-     'user'      => 'mydomain\administrator',
-     'password'  => 'Supersecret#123',
-    }, 
-   dsc_sqlsysadminaccounts => 'mydomain\administrator',
-   dsc_assysadminaccounts  => 'mydomain\administrator',
-   }
+#dsc_sqlsetup {'SqlServerDsc':
+#   dsc_action       =>  'Install',
+#   dsc_instancename =>  'MSSQLSERVER',
+#   dsc_features     =>  'SQLENGINE,AS',
+#   dsc_sourcepath   => $sql_source,
+#   dsc_sourcecredential => {
+#    'user'      =>  'mydomain\administrator',
+#    'password'  =>  'Supersecret#123',
+#    },
+#   dsc_forcereboot  => 'false',
+#   dsc_installshareddir     => 'C:\Program Files\Microsoft SQL Server',
+#   dsc_installsharedwowdir  => 'C:\Program Files (x86)\Microsoft SQL Server',
+#   dsc_instancedir          => 'C:\Program Files\Microsoft SQL Server',
+#   dsc_installsqldatadir    => 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Data',
+#   dsc_sqluserdbdir         => 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Data',
+#   dsc_sqluserdblogdir      => 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Data',
+#   dsc_sqltempdbdir         => 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Data',
+#   dsc_sqltempdblogdir      => 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Data',
+#   dsc_sqlbackupdir         => 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Backup',
+#   dsc_asconfigdir          => 'C:\MSOLAP\Config',
+#   dsc_asdatadir            => 'C:\MSOLAP\Data',
+#   dsc_aslogdir             => 'C:\MSOLAP\Log',
+#   dsc_asbackupdir          => 'C:\MSOLAP\Backup',
+#   dsc_astempdir            => 'C:\MSOLAP\Temp',
+#   dsc_sqlcollation         => 'SQL_Latin1_General_CP1_CI_AS',
+#   dsc_sqlsvcaccount        => {
+#     'user'      => 'mydomain\administrator',
+#     'password'  => 'Supersecret#123',
+#    },
+#   dsc_agtsvcaccount        => {
+#     'user'      => 'mydomain\administrator',
+#     'password'  => 'Supersecret#123',
+#    }, 
+#   dsc_assvcaccount        => {
+#     'user'      => 'mydomain\administrator',
+#     'password'  => 'Supersecret#123',
+#    }, 
+#   dsc_sqlsysadminaccounts => 'mydomain\administrator',
+#   dsc_assysadminaccounts  => 'mydomain\administrator',
+#   }
 
 }
