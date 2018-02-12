e510b1578a615583f20ad6a2ef899b8ffed3dc88
root <root@instance-puppet-enterprise.c.eternal-centaur-185911.internal>
2018-02-12 21:38:23 +0000

    added psrunascreds

--- site/profile/manifests/sql_server_install.pp
+++ site/profile/manifests/sql_server_install.pp
@@ -68,6 +68,7 @@ $sql_source = '\\10.0.10.7\SQLInstallation'
    # }, 
    dsc_sysadminaccounts => 'mydomain\\administrator',
    #dsc_assysadminaccounts  => 'mydomain\administrator',
+   dsc_psdscrunascredential => { 'user' => 'mydomain\\administrator', 'password' => 'Supersecret#123' },
    }
 
 }
