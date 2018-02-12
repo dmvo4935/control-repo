29b5bc16db2fd513f7918f65cf95fca6065b3ec1
root <root@instance-puppet-enterprise.c.eternal-centaur-185911.internal>
2018-02-12 15:44:37 +0000

    commented fields from sqlserverdsc

--- site/profile/manifests/sql_server_install.pp
+++ site/profile/manifests/sql_server_install.pp
@@ -36,20 +36,20 @@ $sql_source = '\\10.0.10.7\SQLInstallation'
     'user'      =>  'mydomain\administrator',
     'password'  =>  'Supersecret#123'
     },
-   dsc_installshareddir     => 'C:\Program Files\Microsoft SQL Server',
-   dsc_installsharedwowdir  => 'C:\Program Files (x86)\Microsoft SQL Server',
-   dsc_instancedir          => 'C:\Program Files\Microsoft SQL Server',
+  # dsc_installshareddir     => 'C:\Program Files\Microsoft SQL Server',
+  # dsc_installsharedwowdir  => 'C:\Program Files (x86)\Microsoft SQL Server',
+  # dsc_instancedir          => 'C:\Program Files\Microsoft SQL Server',
    dsc_installsqldatadir    => 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Data',
    dsc_sqluserdbdir         => 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Data',
    dsc_sqluserdblogdir      => 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Data',
    dsc_sqltempdbdir         => 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Data',
-   dsc_sqltempdblogdir      => 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Data',
-   dsc_sqlbackupdir         => 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Backup',
-   dsc_asconfigdir          => 'C:\MSOLAP\Config',
-   dsc_asdatadir            => 'C:\MSOLAP\Data',
-   dsc_aslogdir             => 'C:\MSOLAP\Log',
-   dsc_asbackupdir          => 'C:\MSOLAP\Backup',
-   dsc_astempdir            => 'C:\MSOLAP\Temp',
+  # dsc_sqltempdblogdir      => 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Data',
+  # dsc_sqlbackupdir         => 'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Backup',
+  # dsc_asconfigdir          => 'C:\MSOLAP\Config',
+  # dsc_asdatadir            => 'C:\MSOLAP\Data',
+  # dsc_aslogdir             => 'C:\MSOLAP\Log',
+  # dsc_asbackupdir          => 'C:\MSOLAP\Backup',
+  # dsc_astempdir            => 'C:\MSOLAP\Temp',
    dsc_sqlcollation         => 'SQL_Latin1_General_CP1_CI_AS',
    dsc_sqladministratorcredential        => {
      'user'      => 'mydomain\administrator',
@@ -63,12 +63,12 @@ $sql_source = '\\10.0.10.7\SQLInstallation'
      'user'      => 'mydomain\administrator',
      'password'  => 'Supersecret#123',
     }, 
-   dsc_assvcaccount        => {
-     'user'      => 'mydomain\administrator',
-     'password'  => 'Supersecret#123',
-    }, 
+   #dsc_assvcaccount        => {
+   #  'user'      => 'mydomain\administrator',
+   #  'password'  => 'Supersecret#123',
+   # }, 
    dsc_sysadminaccounts => 'mydomain\administrator',
-   dsc_assysadminaccounts  => 'mydomain\administrator',
+   #dsc_assysadminaccounts  => 'mydomain\administrator',
    }
 
 }
