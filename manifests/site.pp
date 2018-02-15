adbd9ad4af237f356e7eb4ad451f72414a0dd889
root <root@instance-puppet-enterprise.c.eternal-centaur-185911.internal>
2018-02-15 13:08:28 +0000

    bug fixing

--- manifests/site.pp
+++ manifests/site.pp
@@ -90,11 +90,14 @@ node 'ec2amaz-05d23ld.mydomain.local' {
   
     class { 'profile::create_share': }
 
-    $witness_address="\\\\$::fqdn\\Witness"
+    $witness_address="\\\\$::fqdn\\Witness\\"
 
     @@dsc_xclusterquorum {'Connect witness': 
         dsc_resource    => $witness_address,
-        #dsc_psdscrunascredential        PsDscRunAsCredential
+        dsc_psdscrunascredential  => {
+           'name'     => 'mydomain.local\administrator',
+           'password' => 'Supersecret#123'
+        },
         dsc_issingleinstance  => 'Yes',
         dsc_type              => 'NodeAndFileShareMajority',
    }
