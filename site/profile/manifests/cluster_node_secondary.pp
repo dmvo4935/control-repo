ed6883e7514cff39cdeb2a96962b886cb8b77b8e
root <root@instance-puppet-enterprise.c.eternal-centaur-185911.internal>
2018-02-19 11:00:59 +0000

    added cluster's address to hosts on secondary node

--- site/profile/manifests/cluster_node_secondary.pp
+++ site/profile/manifests/cluster_node_secondary.pp
@@ -9,6 +9,17 @@ windowsfeature { $clustering_features:
 
 exec {'Reset Client DNS Cache': command => 'Clear-DnsClientCache', provider => powershell } ->
 
+file_line {'adding cluster address to hosts file':
+path                   => 'C:\\windows\\system32\\drivers\\etc\\hosts',
+ensure                 => 'present',
+match	               => '^10\.0\.10\.52.*$',
+multiple               => 'false',
+replace                => 'true',
+append_on_no_match     => 'true',
+provider	       => ruby,
+line                   => '10.0.10.52 defaultcluster defaultcluster.mydomain.local'
+} ->
+
   dsc_xwaitforcluster {'WaitForCluster': 
      dsc_retryintervalsec	=> '15',
      dsc_psdscrunascredential	=> {
