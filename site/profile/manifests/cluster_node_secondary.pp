846644561c44c0e65d563fc2afba9bb61bb0625d
root <root@instance-puppet-enterprise.c.eternal-centaur-185911.internal>
2018-02-19 12:11:22 +0000

    added function to convert str to regexp

--- site/profile/manifests/cluster_node_secondary.pp
+++ site/profile/manifests/cluster_node_secondary.pp
@@ -9,16 +9,18 @@ windowsfeature { $clustering_features:
 
 exec {'Reset Client DNS Cache': command => 'Clear-DnsClientCache', provider => powershell } ->
 
-file_line {'adding cluster address to hosts file':
-path                   => 'C:\\windows\\system32\\drivers\\etc\\hosts',
-ensure                 => 'present',
-match	               => '^10\.0\.10\.52.*$',
-multiple               => 'false',
-replace                => 'true',
-append_on_no_match     => 'true',
-provider	       => ruby,
-line                   => '10.0.10.52 defaultcluster defaultcluster.mydomain.local'
-} ->
+File_line <<||>> ->
+
+#file_line {'adding cluster address to hosts file':
+#path                   => 'C:\\windows\\system32\\drivers\\etc\\hosts',
+#ensure                 => 'present',
+#match	               => '^10\.0\.10\.52.*$',
+#multiple               => 'false',
+#replace                => 'true',
+#append_on_no_match     => 'true',
+#provider	       => ruby,
+#line                   => '10.0.10.52 defaultcluster defaultcluster.mydomain.local'
+#} ->
 
   dsc_xwaitforcluster {'WaitForCluster': 
      dsc_retryintervalsec	=> '15',
